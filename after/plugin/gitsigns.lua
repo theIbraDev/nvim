bf7d312233a3e601eb433bb9ab7fc979a9893880
theIbraDev <rubenrossvold@icloud.com>
2024-04-12 00:42:02 +0200

    update

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -28,19 +28,12 @@ require('gitsigns').setup {
         vim.keymap.set({ "n", "x", "o" }, "mh", next_hunk_repeat)
         vim.keymap.set({ "n", "x", "o" }, "Mh", prev_hunk_repeat)
     -- Actions
-    -- Stage current hunk
     map({'n', 'v'}, '<leader>gs', ':Gitsigns stage_hunk<CR>')
-    -- Remove what's new in current hunk
     map({'n', 'v'}, '<leader>gu', ':Gitsigns reset_hunk<CR>')
-    -- Stage undo hunk
     map('n', '<leader>gr', gs.undo_stage_hunk)
-    -- Preview changes in current hunk
     map('n', '<leader>gp', gs.preview_hunk)
-    -- shows last commit for line
     map('n', '<leader>gb', function() gs.blame_line{full=true} end)
-    -- Shows blame for current line
     map('n', '<leader>gc', gs.diffthis)
-    -- shows us older versions, hitting td again will hide it
     map('n', '<leader>gx', gs.toggle_deleted)
 
     -- Text object

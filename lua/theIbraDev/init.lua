74ac62da3b1de5cfc8e88262bf1781bd47338ffe
theIbraDev <rubenrossvold@icloud.com>
2024-05-24 18:12:59 +0200

    update

--- lua/theIbraDev/init.lua
+++ lua/theIbraDev/init.lua
@@ -1,38 +1,39 @@
 require("theIbraDev.set")
 require("theIbraDev.remap")
 require("theIbraDev.lazy_init")
+require("theIbraDev.terminal")
 
 local augroup = vim.api.nvim_create_augroup
-local ibraGroup = augroup('ibraGroup', {})
+local ibraGroup = augroup("ibraGroup", {})
 
 local autocmd = vim.api.nvim_create_autocmd
-local yank_group = augroup('HighlightYank', {})
+local yank_group = augroup("HighlightYank", {})
 
 function R(name)
-    require("plenary.reload").reload_module(name)
+	require("plenary.reload").reload_module(name)
 end
 
 vim.filetype.add({
-    extension = {
-        templ = 'templ',
-    }
+	extension = {
+		templ = "templ",
+	},
 })
 
-autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
+autocmd("TextYankPost", {
+	group = yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
 })
 
-autocmd({"BufWritePre"}, {
-    group = ibraGroup,
-    pattern = "*",
-    command = [[%s/\s\+$//e]],
+autocmd({ "BufWritePre" }, {
+	group = ibraGroup,
+	pattern = "*",
+	command = [[%s/\s\+$//e]],
 })
 
 vim.g.netrw_browse_split = 0

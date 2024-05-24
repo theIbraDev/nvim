1158cd5d0bd30f7a672d34ebcd7a872905ebf55d
theIbraDev <rubenrossvold@icloud.com>
2024-05-24 18:12:22 +0200

    Feat: rust_analyzer properly setup

--- lua/theIbraDev/configs/lsp.lua
+++ lua/theIbraDev/configs/lsp.lua
@@ -1,158 +1,104 @@
--- Lsp
-require("neodev").setup({
-	-- library = {
-	--   plugins = { "nvim-dap-ui" },
-	--   types = true,
-	-- },
-})
-
-local capabilities = nil
-if pcall(require, "cmp_nvim_lsp") then
-	capabilities = require("cmp_nvim_lsp").default_capabilities()
-end
-
-local lspconfig = require("lspconfig")
-
-local servers = {
-	bashls = true,
-	gopls = true,
-	lua_ls = true,
-	rust_analyzer = true,
-	svelte = true,
-	templ = true,
-	cssls = true,
-
-	-- Probably want to disable formatting for this lang server
-	tsserver = true,
-
-	jsonls = {
-		settings = {
-			json = {
-				schemas = require("schemastore").json.schemas(),
-				validate = { enable = true },
-			},
-		},
-	},
-
-	yamlls = {
-		settings = {
-			yaml = {
-				schemaStore = {
-					enable = false,
-					url = "",
-				},
-				schemas = require("schemastore").yaml.schemas(),
-			},
-		},
-	},
-
-	ocamllsp = {
-		manual_install = true,
-		settings = {
-			codelens = { enable = true },
-		},
-
-		filetypes = {
-			"ocaml",
-			"ocaml.interface",
-			"ocaml.menhir",
-			"ocaml.cram",
-		},
-	},
-
-	lexical = {
-		cmd = { "/home/tjdevries/.local/share/nvim/mason/bin/lexical", "server" },
-		root_dir = require("lspconfig.util").root_pattern({ "mix.exs" }),
-	},
-
-	clangd = {
-		-- TODO: Could include cmd, but not sure those were all relevant flags.
-		--    looks like something i would have added while i was floundering
-		init_options = { clangdFileStatus = true },
-		filetypes = { "c" },
-	},
-}
-
-local servers_to_install = vim.tbl_filter(function(key)
-	local t = servers[key]
-	if type(t) == "table" then
-		return not t.manual_install
-	else
-		return t
-	end
-end, vim.tbl_keys(servers))
+local cmp_lsp = require("cmp_nvim_lsp")
+local util = require("lspconfig/util")
+local capabilities = vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities(), cmp_lsp.default_capabilities())
+local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
 
 require("mason").setup()
-local ensure_installed = {
-	"stylua",
-	"lua_ls",
-	"delve",
-	--"tailwind-language-server",
-}
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"lua_ls",
+		"rust_analyzer",
+		"gopls",
+		"svelte",
+		"tsserver",
+		"eslint",
+	},
+	handlers = {
+		function(server_name) -- default handler (optional)
+			require("lspconfig")[server_name].setup({
+				capabilities = capabilities,
+			})
+		end,
 
-vim.list_extend(ensure_installed, servers_to_install)
-require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+		["lua_ls"] = function()
+			local lspconfig = require("lspconfig")
 
-for name, config in pairs(servers) do
-	if config == true then
-		config = {}
-	end
-	config = vim.tbl_deep_extend("force", {}, {
-		capabilities = capabilities,
-	}, config)
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						runtime = { version = "Lua 5.1" },
+						diagnostics = {
+							globals = { "vim", "it", "describe", "before_each", "after_each" },
+						},
+					},
+				},
+			})
 
-	lspconfig[name].setup(config)
-end
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+                on_attach = function(client, bufnr)
+                    vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+                end,
+                filetypes = {"rust"},
+                root_dir = util.root_pattern("Cargo.toml"),
+			})
 
-local disable_semantic_tokens = {
-	lua = true,
-}
+		end,
+	},
+})
 
-local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(args)
-		local bufnr = args.buf
-		local client = assert(vim.lsp.get_client_by_id(args.data.client_id), "must have valid client")
+vim.diagnostic.config({
+	-- update_in_insert = true,
+	float = {
+		focusable = false,
+		style = "minimal",
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+	},
+})
+
+local autocmd = vim.api.nvim_create_autocmd
+
+local ibraGroup = vim.api.nvim_create_augroup("theIbraDev_worktree", {})
+
+autocmd("LspAttach", {
+	group = ibraGroup,
+	callback = function(e)
+		local opts = { buffer = e.buf } vim.keymap.set("n", "gd", function() vim.lsp.buf.definition()
+		end, opts) vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+		vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+		vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+		vim.keymap.set("n", "gR", function() vim.lsp.buf.rename() end, opts)
+		vim.keymap.set("n", "gq", function() vim.lsp.buf.workspace_symbol() end, opts)
+		vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+		vim.keymap.set("n", "gT", function() vim.lsp.buf.type_definition() end, opts)
+		vim.keymap.set("n", "ga", function() vim.lsp.buf.code_action() end, opts)
+
 		local next_diagnostic_repeat, prev_diagnostic_repeat =
 			ts_repeat_move.make_repeatable_move_pair(vim.diagnostic.goto_next, vim.diagnostic.goto_prev) -- Makes moves repeatable
-
-		--old
-		vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
-		vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, { buffer = 0 })
 		vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_next) -- moves regardless of the last direction
 		vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_previous)
 		vim.keymap.set({ "n", "x", "o" }, "md", next_diagnostic_repeat) -- Has to be below repeatable pair
 		vim.keymap.set({ "n", "x", "o" }, "Md", prev_diagnostic_repeat)
-		--new
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = 0 })
-		vim.keymap.set("n", "gR", vim.lsp.buf.rename, { buffer = 0 })
-		vim.keymap.set("n", "gq", vim.lsp.buf.workspace_symbol)
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
-		vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = 0 })
-		vim.keymap.set("n", "ga", vim.lsp.buf.code_action, { buffer = 0 })
-
-		local filetype = vim.bo[bufnr].filetype
-		if disable_semantic_tokens[filetype] then
-			client.server_capabilities.semanticTokensProvider = nil
-		end
 	end,
 })
 
--- Autoformatting Setup
 require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
+		-- Conform will run multiple formatters sequentially
+		python = { "isort", "black" },
+		-- Use a sub-list to run only the first available formatter
+		javascript = { { "prettierd", "prettier" } },
 	},
 })
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-	callback = function(args)
-		require("conform").format({
-			bufnr = args.buf,
-			lsp_fallback = true,
-			quiet = true,
-		})
-	end,
-})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+-- 	pattern = "*",
+-- 	callback = function(args)
+-- 		require("conform").format({ bufnr = args.buf })
+-- 	end,
+-- })
--- lua/theIbraDev/plugins/lsp.lua
+++ lua/theIbraDev/plugins/lsp.lua
@@ -1,19 +1,15 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		{ "j-hui/fidget.nvim", opts = {} }, -- Shows LSP activity on the bottom right
-		"nvim-treesitter/nvim-treesitter-textobjects", -- Extends tresitter selection
-		"folke/neodev.nvim",
-		-- Autoformatting
-		"stevearc/conform.nvim",
-		-- Schema information
-		"b0o/SchemaStore.nvim",
-	},
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        { "j-hui/fidget.nvim", opts = {} }, -- Shows LSP activity on the bottom right
+        "nvim-treesitter/nvim-treesitter-textobjects", -- Extends tresitter selection
+        "stevearc/conform.nvim",
+        "rust-lang/rust.vim",
+    },
 
-	config = function()
-		require("theIbraDev.configs.lsp")
-	end,
+    config = function()
+        require("theIbraDev.configs.lsp")
+    end,
 }

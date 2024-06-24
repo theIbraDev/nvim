7d6145832668d0da767153f5c273d5d950450ca1
theIbraDev <ruben@icloud.com>
2024-06-25 00:22:44 +0200

    new keybind for fugitifve

--- lua/theIbraDev/configs/fugitive.lua
+++ lua/theIbraDev/configs/fugitive.lua
@@ -19,6 +19,7 @@ autocmd("BufWinEnter", {
 		vim.keymap.set("n", "<leader>gp", ":Git push -u origin ", opts)
 		vim.keymap.set("n", "<leader>go", ":Git remote add origin ", opts)
 		vim.keymap.set("n", "<leader>gr", ":Git rebase ", opts)
+		vim.keymap.set("n", "gæ", "Git rebase --interactive", opts)
 
 		vim.keymap.set("n", "<leader>p", function()
 			vim.cmd([[ Git push ]])

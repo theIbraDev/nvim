db0a2638fd1bb91818c9a278549c707cf29bbbe8
theIbraDev <rubenrossvold@icloud.com>
2024-09-16 19:34:57 +0200

    High contrast rosepine

--- lua/theIbraDev/plugins/theme.lua
+++ lua/theIbraDev/plugins/theme.lua
@@ -10,7 +10,21 @@ return {
 				styles = {
 					bold = true,
 					italic = false,
-					transparency = true,
+					transparency = false,
+				},
+
+				palette = {
+					-- Override the builtin palette per variant
+					moon = {
+						base = "#191724", -- Base of main
+						surface = "#1f1d2e", -- Surface of main
+						overlay = "#26233a", -- Overlay of main
+						gold = "#ea9d34", -- Gold of dawn
+						rose = "#d7827e", -- Rose of dawn
+						muted = "#6e6a86",
+						sublte = "#9e9aba",
+						iris = "#c4a7e7",
+					},
 				},
 			})
 			vim.cmd("colorscheme rose-pine-moon")

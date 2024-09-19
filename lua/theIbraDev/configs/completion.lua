1422ec6d289d8bf4104c9b0996ac7d35ba3f2662
theIbraDev <rubenrossvold@icloud.com>
2024-09-19 20:22:27 +0200

    cmp search and command line

--- lua/theIbraDev/configs/completion.lua
+++ lua/theIbraDev/configs/completion.lua
@@ -1,4 +1,4 @@
-require "theIbraDev.configs.snippets"
+require("theIbraDev.configs.snippets")
 
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
 vim.opt.shortmess:append("c")
@@ -7,10 +7,10 @@ local lspkind = require("lspkind")
 lspkind.init({})
 
 local cmp = require("cmp")
-local luasnip = require('luasnip')
+local luasnip = require("luasnip")
 
 require("luasnip.loaders.from_vscode").lazy_load()
-luasnip.config.setup {}
+luasnip.config.setup({})
 
 cmp.setup({
 	-- Enable luasnip to handle snippet expansion for nvim-cmp
@@ -20,12 +20,12 @@ cmp.setup({
 			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
 			-- can also be a function to dynamically calculate max width such as
 			-- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-			ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+			ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
 			show_labelDetails = true, -- show labelDetails in menu. Disabled by default
 			symbol_map = {
 				Copilot = "",
 			},
-		})
+		}),
 	},
 	snippet = {
 		expand = function(args)
@@ -33,8 +33,8 @@ cmp.setup({
 		end,
 	},
 	window = {
-		-- completion = cmp.config.window.bordered(),
-		-- documentation = cmp.config.window.bordered(),
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
 	},
 	mapping = {
 		["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
@@ -48,19 +48,42 @@ cmp.setup({
 		),
 	},
 	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
+		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
-		{ name = 'copilot' },
 		{ name = "path" },
 	}, {
-			{ name = 'buffer' },
-		})
+		{ name = "buffer" },
+		{ name = "copilot" },
+	}),
 })
 
 -- Setup up vim-dadbod
 cmp.setup.filetype({ "sql" }, {
-    sources = {
-        { name = "vim-dadbod-completion" },
-        { name = "buffer" },
-    },
+	sources = {
+		{ name = "vim-dadbod-completion" },
+		{ name = "buffer" },
+	},
+})
+
+-- `/` cmdline setup.
+cmp.setup.cmdline("/", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
+})
+
+-- `:` cmdline setup.
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{
+			name = "cmdline",
+			option = {
+				ignore_cmds = { "Man", "!" },
+			},
+		},
+	}),
 })

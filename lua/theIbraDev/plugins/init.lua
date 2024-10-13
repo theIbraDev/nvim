4eda782f1e4ffbd55dc9a86528213c1823071eb0
theIbraDev <rubenrossvold@icloud.com>
2024-10-14 01:33:40 +0200

    remove deprecated, remove bg

--- lua/theIbraDev/plugins/init.lua
+++ lua/theIbraDev/plugins/init.lua
@@ -4,15 +4,6 @@ return {
         name = "plenary"
     },
     {
-        'ThePrimeagen/vim-be-good',
-    },
-    {
-        "nvim-treesitter/playground",
-    },
-    {
         'nvim-tree/nvim-web-devicons'
-    },
-    {
-        'evanleck/vim-svelte',
     },
 }
--- lua/theIbraDev/plugins/theme.lua
+++ lua/theIbraDev/plugins/theme.lua
@@ -10,7 +10,7 @@ return {
 				styles = {
 					bold = true,
 					italic = false,
-					transparency = false,
+					transparency = true,
 				},
 
 				palette = {
